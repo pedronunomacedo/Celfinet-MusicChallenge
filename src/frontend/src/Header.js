89284e2dff75a44796f39dfd912ea329680bcd48
Diogo filipe Pereira santos <dp81386@corp.cyient.com>
2024-08-01 18:48:23 +0100

    slogan fix

--- src/frontend/src/Header.js
+++ src/frontend/src/Header.js
@@ -4,7 +4,7 @@ import './Header.css';
 const Header = () => (
     <header className="header">
             <h1 className="header-title">Celfinet Timeless</h1>
-            <h2 className="header-subtitle">Ever wondered what memories feel like?</h2>
+            <h2 className="header-subtitle">Ever wondered what memories sound like?</h2>
     </header>
 );
 
